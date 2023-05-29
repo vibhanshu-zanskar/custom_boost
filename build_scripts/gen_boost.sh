160bd744e2a6e6cb0c9f4c5a9cd07c51835e17b2
Vibhanshu Prasad <vibhanshu@zanskar.xyz>
2023-05-29 21:19:18 +0530

    yintegrate repo

--- build_scripts/gen_boost.sh
+++ build_scripts/gen_boost.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-echo "Generating boost libs"
+echo "## RUN SCRIPT: Generating boost libs"
 
 SCRIPT_PATH=$(realpath "$0")
 REPO_PATH=$(dirname $(dirname $SCRIPT_PATH))
@@ -10,6 +10,7 @@ STAGING_AREA=${REPO_PATH}/staging/
 mkdir -p ${STAGING_AREA}
 
 BOOST_LIB_NAME=boost_1_81_0
+echo "# Download boost archive ${BOOST_LIB_NAME}"
 BOOST_HTTPS_PATH=https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/${BOOST_LIB_NAME}.tar.gz
 wget -q --show-progress "${BOOST_HTTPS_PATH}" -P ${STAGING_AREA}
 tar zxf ${STAGING_AREA}/${BOOST_LIB_NAME}.tar.gz -C ${STAGING_AREA}
@@ -25,6 +26,7 @@ fi
 pushd ${BOOST_DIR}
 mkdir -p build
 
+echo "# Build Boost and bcp"
 bash ./bootstrap.sh --prefix=${BOOST_DIR}/build
 ./b2
 ./b2 headers
@@ -39,7 +41,10 @@ then
     exit 1
 fi
 
-eval "${BCP_PATH} algortihm asio boost/circular_buffer.hpp boost/config.hpp container functional hana io iterator boost/lexical_cast.hpp mp11 boost/operators.hpp preprocessor process program_options system property_tree range boost/scope_exit.hpp test build bootstrap.sh boostcpp.jam boost-build.jam --namespace=zr_boost_1_81 --boost=${BOOST_DIR} ${REPO_PATH}/zr_boost_1_81"
+OUTPUT_DIR=${REPO_PATH}/zr_boost_1_81
+echo "# Run bcp: output dir ${OUTPUT_DIR}"
+mkdir -p ${OUTPUT_DIR}
+eval "${BCP_PATH} algortihm asio boost/circular_buffer.hpp boost/config.hpp container functional hana io iterator boost/lexical_cast.hpp mp11 boost/operators.hpp preprocessor process program_options system property_tree range boost/scope_exit.hpp test build bootstrap.sh boostcpp.jam boost-build.jam --namespace=zr_boost_1_81 --boost=${BOOST_DIR} ${OUTPUT_DIR}"
 
 # Directory to be included to compiler include path:  ${BOOST_DIR}
 # Directory to be included to linker library path:  ${BOOST_DIR}/stage/lib
