935cf71b7a6606b6c1502afb2d35c7a6fe137814
Vibhanshu Prasad <vibhanshu@zanskar.xyz>
2023-06-24 13:01:39 +0530

    add more libs

---
+++ zr_boost_1_81/libs/date_time/test/posix_time/compile_fail/hours_special_value.cpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2017 James E. King III
+// Use, modification and distribution is subject to the
+// Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/core/ignore_unused.hpp>
+#include <boost/date_time/posix_time/posix_time_duration.hpp>
+#include <boost/date_time/special_defs.hpp>
+
+int main()
+{
+    using zr_boost_1_81::date_time::pos_infin;
+    using zr_boost_1_81::posix_time::hours;
+    zr_boost_1_81::ignore_unused(hours(static_cast<zr_boost_1_81::int64_t>(pos_infin))); // compiles: it's an integral
+    zr_boost_1_81::ignore_unused(hours(pos_infin));                              // won't compile: not an integral
+    return 1;                                                            // return an error if we actually compile
+}
