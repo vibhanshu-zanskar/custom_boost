4da122fc3e6ae3adc681b1ce26d39985e967e785
Vibhanshu Prasad <vibhanshu@zanskar.xyz>
2023-06-24 19:05:26 +0530

    add boost logger and interprocess until it is removed from CoreUtil codebaase

---
+++ zr_boost_1_81/boost/interprocess/detail/move.hpp
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Ion Gaztanaga 2010-2012.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/move for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+//! \file
+
+#ifndef BOOST_INTERPROCESS_DETAIL_MOVE_HPP
+#define BOOST_INTERPROCESS_DETAIL_MOVE_HPP
+
+#ifndef BOOST_CONFIG_HPP
+#  include <boost/config.hpp>
+#endif
+#
+#if defined(BOOST_HAS_PRAGMA_ONCE)
+#  pragma once
+#endif
+
+#include <boost/move/utility_core.hpp>
+
+namespace zr_boost_1_81 {
+namespace interprocess {
+
+using ::zr_boost_1_81::move;
+using ::zr_boost_1_81::forward;
+
+}  //namespace interprocess {
+}  //namespace zr_boost_1_81 {
+
+#endif //#ifndef BOOST_INTERPROCESS_DETAIL_MOVE_HPP
